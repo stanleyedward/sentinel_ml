01300c6cfa8556bf63ae1a674b338be152637f0f
stanleyedward <stanleyedward9715@gmail.com>
2024-03-09 21:41:41 +0530

    finish training

--- pytorch_modules/utils.py
+++ pytorch_modules/utils.py
@@ -206,22 +206,23 @@ def plot_loss_curves(results):
     accuracy = results["train_acc"]
     test_accuracy = results["test_acc"]
 
-    epochs = range(len(results["train_loss"]))
+    train_epochs = range(len(results["train_loss"]))
+    test_epochs = range(len(results["test_loss"]))
 
     plt.figure(figsize=(15, 7))
 
     # Plot loss
     plt.subplot(1, 2, 1)
-    plt.plot(epochs, loss, label="train_loss")
-    plt.plot(epochs, test_loss, label="test_loss")
+    plt.plot(train_epochs, loss, label="train_loss")
+    plt.plot(test_epochs, test_loss, label="test_loss")
     plt.title("Loss")
     plt.xlabel("Epochs")
     plt.legend()
 
     # Plot accuracy
     plt.subplot(1, 2, 2)
-    plt.plot(epochs, accuracy, label="train_accuracy")
-    plt.plot(epochs, test_accuracy, label="test_accuracy")
+    plt.plot(train_epochs, accuracy, label="train_accuracy")
+    plt.plot(test_epochs, test_accuracy, label="test_accuracy")
     plt.title("Accuracy")
     plt.xlabel("Epochs")
     plt.legend()
