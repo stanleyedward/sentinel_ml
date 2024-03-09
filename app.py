cdc222da71888061eb44ea385bab6dd41429a1fd
stanleyedward <stanleyedward9715@gmail.com>
2024-03-09 23:35:39 +0530

    return class names

--- app.py
+++ app.py
@@ -9,16 +9,21 @@ from typing import List
 app = FastAPI()
 model = inference_utils.load_model(config.MODEL_DIR)
 tokenizer = DistilBertTokenizer.from_pretrained(config.TOKENIZER_DIR, truncation=True, do_lower_case=True)
+class_names = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+class_mapping = {index: class_name for index, class_name in enumerate(class_names)}
 
 class TextInput(BaseModel):
     text: str
 
 class SentimentOutput(BaseModel):
-    sentiment: List[int]
+    sentiment: List[str]
 
 @app.post("/predict/", response_model=SentimentOutput)
 async def predict(text_input: TextInput):
     tokenized_text = inference_utils.clean_and_tokenize_text(text_input.text, tokenizer)
     pred = inference_utils.inference_step(tokenized_text, model)
     
-    return {"sentiment": pred}
+    class_mapping = {index: class_name for index, class_name in enumerate(class_names)}
+    flags = [class_mapping[index] for index, value in enumerate(pred) if value == 1]
+    
+    return {"sentiment": flags}
