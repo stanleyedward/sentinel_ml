7faa50d2c23c8d094ad99cfccd9c90790756a6bc
stanleyedward <stanleyedward9715@gmail.com>
2024-03-09 23:13:13 +0530

    update app

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from transformers import DistilBertTokenizer
 from pytorch_modules import inference_utils, config
 from typing import List
 
+
 app = FastAPI()
 model = inference_utils.load_model(config.MODEL_DIR)
 tokenizer = DistilBertTokenizer.from_pretrained(config.TOKENIZER_DIR, truncation=True, do_lower_case=True)
@@ -17,7 +18,7 @@ class SentimentOutput(BaseModel):
 
 @app.post("/predict/", response_model=SentimentOutput)
 async def predict(text_input: TextInput):
-    tokenized_text = inference_utils.clean_and_tokenize_text(text_input, tokenizer)
+    tokenized_text = inference_utils.clean_and_tokenize_text(text_input.text, tokenizer)
     pred = inference_utils.inference_step(tokenized_text, model)
     
     return {"sentiment": pred}
