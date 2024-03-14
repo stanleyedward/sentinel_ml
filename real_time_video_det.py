45cb3be2743490ff1a0dcc1d481bc7bf1b47af63
stanleyedward <stanleyedward9715@gmail.com>
2024-03-14 23:30:02 +0530

    add break after flag and fps

--- real_time_video_det.py
+++ real_time_video_det.py
@@ -3,15 +3,18 @@ import cv2
 from NudeNet import nudenet
 import os
 
-# camera = cv2.VideoCapture(0) #webcam
-detector = nudenet.NudeDetector()
-model_fps = 14 # low ball
+
+videeo_detector = nudenet.NudeDetector()
+video_model_fps = 14 # low ball
+video_classes_to_detect = ['FACE_MALE']
     
 def video_detection(path, fps: int, output_dir = None, break_after_flag:bool = False):
+    assert fps >= 1, "fps cannot be lower than 1"
+    
     cap = cv2.VideoCapture(path)
     frame_count = 0
-    assert fps >= 1, "fps cannot be lower than 1"
-    skip_frames = -(-fps // model_fps) #ceiling
+    skip_frames = -(-fps // video_model_fps) #ceiling
+    # skip_frames = fps // video_model_fps #floor
     
     while cap.isOpened():
         ret, frame = cap.read()
@@ -19,10 +22,11 @@ def video_detection(path, fps: int, output_dir = None, break_after_flag:bool = F
             break
         
         if frame_count % skip_frames == 0:
-            frame_prediction = detector.detect(frame)
+            frame_prediction = videeo_detector.detect(frame)
             print(f"Frame {frame_count}: Predicted class: {frame_prediction}")
             
-            if break_after_flag and frame_prediction:
+            classes_detected = [detection for detection in frame_prediction if detection["class"] in video_classes_to_detect]
+            if break_after_flag and classes_detected:
                 break
             else:
                 pass
@@ -36,6 +40,6 @@ def video_detection(path, fps: int, output_dir = None, break_after_flag:bool = F
     cap.release()
     
 if __name__ == '__main__':
-    # video_detection('video_samples/dexter.mp4', 30)
-    video_detection('image_samples/booba.jpg', 1)
+    video_detection('video_samples/me_in_toilet.mp4', fps = 30, output_dir=None, break_after_flag=True)
+    # video_detection(0, fps = 30, output_dir=None, break_after_flag=True) for webcam 
 
